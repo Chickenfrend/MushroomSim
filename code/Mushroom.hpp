2de9416c6315bca0d8d356a7d8241dddd94fe0fb
Benjamin Lain <chickenfrend@gmail.com>
2020-12-09 17:32:02 -0800

    Made some more changes, added shroom tracker

--- code/Mushroom.hpp
+++ code/Mushroom.hpp
@@ -2,13 +2,16 @@
 #define __MUSHROOM
 
 #include <string> 
+#include "ShroomTracker.hpp"
 class Mushroom{
-	private:
-		float density;
-		int reproductiveAge;
-		bool isBlooming;
-		std::string description;
-		std::string name;
+	protected:
+		float density; //The density of the mycelium in whatever ground block (should this be kept in the ground object?).
+		int reproductiveAge; //The age at which the mycelium starts to produce mushrooms.
+		bool isBlooming; //Whether or not the mycelium is making mushrooms
+		std::string description; //The description of the mushroom. Possibly, this could be stored in the shroom tracker object to avoid unecessarily repeated storage of information. 
+		std::string name; //The name of the mushrooms species.
+		std::unique_ptr<ShroomTracker> organism; // A pointer to a shroom tracker object, which keeps track of what larger organism this mycelium block is part of.
+
 	public:
 		virtual bool checkSpreadConditions();
 		virtual void spread();
@@ -18,6 +21,9 @@ class Mushroom{
 		bool getBloomingStatus();
 		std::string getDescription();
 		std::string getName();
+		std::unique_ptr<ShroomTracker> getShroomTracker();
+
+		void setShroomTracker(unique_ptr<ShroomTracker> tracker);
 };
 
 #endif
---
+++ code/ShroomTracker.hpp
@@ -0,0 +1,14 @@
+#ifndef __SHROOMTRACKER
+#define __SHROOMTRACKER
+
+//The shroom tracker acts like the trunk does for tree roots, but for mycelium. 
+//Since mushrooms don't have a trunk, or a point of origin, this is abstract and does not have a place in the world.
+//Instead, if two Mushroom objects share the shame ShroomTracker, they are considered to be part of the same mushroom organism.
+class ShroomTracker{
+	int id;
+	int age;
+	int count;
+	float mass;
+}
+
+#endif
