44eb4b1da9438acfe2e1fe55f6438cb71e644f6b
BenAtLittleThinker <chickenfrend@gmail.com>
2021-01-14 21:48:36 -0800

    just, copied over the treegenerator to shroomgenerator

---
+++ code/RandShroomGenerator.cpp
@@ -0,0 +1,84 @@
+#include "RandTreeGenerator.hpp"
+
+RandTreeGenerator::RandTreeGenerator(std::string _forestPath, std::shared_ptr<WorldState> currentState){
+	forestPath = _forestPath;
+	worldState = currentState;
+	GenerateLists();
+}
+
+void RandTreeGenerator::GenerateLists(){
+	std::ifstream treeFile(forestPath);
+
+	std::string line;
+	std::string firstLine;
+	std::string treeName;
+	int weight;
+
+	if(!treeFile.is_open()){
+		throw std::runtime_error("Could not open forest file");
+	}
+
+	if(treeFile.good()){
+		std::getline(treeFile, firstLine);
+        std::cout << "Got Here" << std::endl;
+	}else{
+        throw std::runtime_error("File is not good");
+    }
+	
+	int index = 0;
+
+	int weightSum = 0;
+	while(std::getline(treeFile, line)){
+		int currentWeight;
+		std::istringstream stream(line);
+        std::string tempString = "";
+		std::getline(stream, tempString, ',');
+        treeNames.push_back(tempString);
+
+		stream >> currentWeight;
+
+		if(index == 0){
+			intervals.push_back(currentWeight);
+		}else{
+			intervals.push_back(intervals[index-1] + currentWeight);
+		}
+
+		index++;	
+	}
+
+    for(int i = 0; i < treeNames.size(); i++){
+        std::cout << treeNames[i] << " ";
+    }
+    std::cout << std::endl;
+    for(int i = 0; i < intervals.size(); i++){
+        std::cout << intervals[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+}
+
+std::string RandTreeGenerator::pickRandom(){
+	int randInt = rand() % intervals.back();
+	for(int i = 0; i < intervals.size(); i++){
+	 	std::cout << "Got to for loop" << std::endl;	
+		if(randInt < intervals[i]){
+			std::cout << "Current interval is " << intervals[i] << std::endl;
+			return treeNames[i]; 
+		}
+	}
+	throw std::runtime_error("Failed to pick a random tree. Bad or empty forest file?");
+}
+
+TreeTrunk* RandTreeGenerator::generateTreeTrunk(){
+	std::string randTreeName = pickRandom();
+	if(randTreeName == "OakTrunk"){
+		return new OakTrunk(worldState);
+	}else if(randTreeName == "PineTrunk"){
+		return new PineTrunk(worldState);
+	}else{
+		std::string error = "Invalid tree name " + randTreeName;
+		throw std::runtime_error(error);
+		return new TreeTrunk(worldState);
+	}
+}
---
+++ code/RandShroomGenerator.hpp
@@ -0,0 +1,26 @@
+#ifndef __RANDOMTREEGENERATOR
+#define __RANDOMTREEGENERATOR
+#include "OakTrunk.hpp"
+#include "PineTrunk.hpp"
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <sstream>
+#include <iostream>
+
+class RandTreeGenerator{
+	public:
+		RandTreeGenerator(std::string _forestPath, std::shared_ptr<WorldState> currentState);
+		//This function allocates memory! If you call it you must either encapsulate the pointer
+		//with a smart pointer or remember to delete it.
+		TreeTrunk* generateTreeTrunk();
+	private:
+		std::string forestPath;
+		void GenerateLists();
+		std::string pickRandom();
+		std::vector<std::string> shroomNames;
+		std::vector<int> intervals;
+		std::shared_ptr<WorldState> worldState;
+};
+
+#endif
